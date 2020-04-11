fcb8b3126e175c9dbbac50e7fd52070dd8942f62
jinyu <jwu427@wisc.edu>
2020-04-10 23:03:11 -0500

    sovled merges conflict

--- src/test/java/org/wisc/business/service/AuthenticationServiceTest.java
+++ src/test/java/org/wisc/business/service/AuthenticationServiceTest.java
@@ -2,12 +2,17 @@ package org.wisc.business.service;
 
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.wisc.business.model.UserModel.User;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 class AuthenticationServiceTest {
 
     @Autowired
@@ -18,17 +23,12 @@ class AuthenticationServiceTest {
         User testU = User.builder().email("badger@wisc.edu").password("123456").build();
         String token = authenticationService.login(testU);
         Assert.assertThat(authenticationService.isValidToken(token), is(true));
+        User testU2 = User.builder().email("badger@wisc.edu").password("111111").build();
+        String token2 = authenticationService.login(testU2);
+        Assert.assertThat(authenticationService.isValidToken(token2), is(false));
     }
 
     @Test
     void isValidToken() {
-    }
-
-    @Test
-    void getCurrentUser() {
-    }
-
-    @Test
-    void isLoggedIn() {
     }
 }
