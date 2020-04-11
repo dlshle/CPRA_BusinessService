353eb84d0069e9dd9686800e7c2828d3be993b70
jinyu <jwu427@wisc.edu>
2020-04-10 23:14:09 -0500

    Finished user authentication test

--- src/test/java/org/wisc/business/service/AuthenticationServiceTest.java
+++ src/test/java/org/wisc/business/service/AuthenticationServiceTest.java
@@ -9,7 +9,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.wisc.business.model.UserModel.User;
 
 import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -22,13 +21,21 @@ class AuthenticationServiceTest {
     void login() {
         User testU = User.builder().email("badger@wisc.edu").password("123456").build();
         String token = authenticationService.login(testU);
-        Assert.assertThat(authenticationService.isValidToken(token), is(true));
+        Assert.assertThat("valid user login test", authenticationService.isValidToken(token), is(true));
         User testU2 = User.builder().email("badger@wisc.edu").password("111111").build();
         String token2 = authenticationService.login(testU2);
-        Assert.assertThat(authenticationService.isValidToken(token2), is(false));
+        Assert.assertThat("invalid user login test", authenticationService.isValidToken(token2), is(false));
     }
 
     @Test
     void isValidToken() {
+        final String validToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9" +
+                ".eyJhdWQiOiI1ZThmZWYwYzk2NjM1YTY1ZGVhYTRlYjgiLCJpYXQiOjE1ODY1NzYxNTZ9.5VXIoy2IpcwP5r8ig6UxcsYKcJGRCZ1_3bGAufLHayU";
+        final String validTokenMessage = "Test with a valid user Token";
+        final String inValidToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9" +
+                ".eyJhdWQiOiI1ZThmZWYwYzk2NjM1YTY1ggVhYTRlYjgiLCJpYXQiOjE1ODY1NzYxNTZ9.5VXIoy2IpcwP5r8ig6UxcsYKcJGRCZ1_3bGAufLHayU";
+        final String inValidTokenMessage = "Test with invalid Token";
+        Assert.assertThat(validTokenMessage, authenticationService.isValidToken(validToken), is(true));
+        Assert.assertThat(inValidTokenMessage, authenticationService.isValidToken(inValidToken), is(false));
     }
 }
